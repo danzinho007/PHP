e4f40ddaf27af649c7b98541fced82dd8d7d6303
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-23 01:52:50 -0200

    adicionei

---
+++ aula02.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title></title>
+	<style>
+		
+	</style>
+</head>
+<body>
+<div>
+	<?php
+		$n = 4;
+		$n = 4.5;
+		$no = "Daniel";
+		$no1 = (string)"Daniel";
+		$no2 = (int)"Daniel";
+		$no3 = (int)"10Daniel";
+		$no4 = (int)"Daniel10";
+		echo $n . "<br>";
+		echo $no . "<br>"; 
+		echo $no1 . "<br>";
+		echo $no2 . "<br>";
+		echo $no3 . "<br>";
+		echo $no4 . "<br>"
+	?>
+</div>
+</body>
+</html>
